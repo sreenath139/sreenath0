95599c6fb93523706342457cf721116d5cb24b99
sreenath139 <53077642+sreenath139@users.noreply.github.com>
2019-07-19 15:45:11 +0530

    Update and rename vowelss.py to 12345.py

---
+++ 12345.py
@@ -0,0 +1,5 @@
+num=str(input())
+if (num>='a' and num<='z') or (num>='A' and num<='Z'):
+    print('Alphabet')
+else:
+    print('No')
--- vowelss.py
+++
@@ -1,9 +0,0 @@
-ch = input("")
-if((ch>='a' and ch<= 'z') or (ch>='A' and ch<='Z')):
-  if(ch=='a' or ch=='A' or ch=='E' or ch=='e' or ch=='i' or ch=='O' or ch=='o' or ch=='I' or ch=='U' or ch=='u'):
-    print("Vowel")
-  else:
-    print("Consonant")      
-else:
-  print("invalid")
-
