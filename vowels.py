4e55845dfc62174e3ccb7d63ff92db1056d474f8
sreenath139 <53077642+sreenath139@users.noreply.github.com>
2019-07-19 15:30:46 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,13 +1,9 @@
-vowels=['a','e','i','o','u']
-consonants=['b','c','d','f','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
-ch=input("enter a alphate")
-a=ch.lower()
-if a in vowels:
-    print('vowel')
-elif(a in consonants):
-    print('consonants')
-else :
-    print('invalid')
-    
-            
-        
+ch = input("")
+if((ch>='a' and ch<= 'z') or (ch>='A' and ch<='Z')):
+  if(ch=='a' or ch=='A' or ch=='E' or ch=='e' or ch=='i' or ch=='O' or ch=='o' or ch=='I' or ch=='U' or ch=='u'):
+    print("Vowel")
+  else:
+    print("Consonant")      
+else:
+  print("invalid")
+
